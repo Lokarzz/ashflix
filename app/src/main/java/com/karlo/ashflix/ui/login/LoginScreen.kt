44517059d9b442553ed1c9e621eed78dd6b83ecc
Lokarz <karloechavez@gmail.com>
2024-11-07 14:37:13 +0800

    feat: login and sign up button

--- app/src/main/java/com/karlo/ashflix/ui/login/LoginScreen.kt
+++ app/src/main/java/com/karlo/ashflix/ui/login/LoginScreen.kt
@@ -1,12 +1,15 @@
 package com.karlo.ashflix.ui.login
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,9 +30,10 @@ fun LoginScreen(modifier: Modifier = Modifier, onLoginSuccess: () -> Unit) {
             modifier = Modifier
                 .align(Alignment.Center)
                 .padding(horizontal = dimensionResource(R.dimen.padding_small))
-                .fillMaxWidth()
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
         ) {
-            Logo(modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium)))
+            Logo()
             AppOutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 label = stringResource(R.string.username)
@@ -39,9 +43,7 @@ fun LoginScreen(modifier: Modifier = Modifier, onLoginSuccess: () -> Unit) {
                 label = stringResource(R.string.password),
                 visualTransformation = PasswordVisualTransformation()
             )
-            Button(onClick = onLoginSuccess) {
-                Text("login!")
-            }
+            LoginSignUp(modifier = Modifier.fillMaxWidth(), onLoginSuccess = onLoginSuccess)
         }
 
     }
@@ -54,6 +56,21 @@ private fun Logo(modifier: Modifier = Modifier) {
         text = stringResource(R.string.app_name),
         style = MaterialTheme.typography.displayLarge
     )
+}
+
+@Composable
+private fun LoginSignUp(modifier: Modifier = Modifier, onLoginSuccess: () -> Unit) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+    ) {
+        Button(modifier = Modifier.weight(1f), onClick = onLoginSuccess) {
+            Text(text = stringResource(R.string.login))
+        }
+        OutlinedButton(modifier = Modifier.weight(1f), enabled = false, onClick = onLoginSuccess) {
+            Text(text = stringResource(R.string.sign_up))
+        }
+    }
 }
 
 
